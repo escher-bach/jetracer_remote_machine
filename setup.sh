4374970c8ef352a13e084e34f733e8e0333f48d2
Aniruddha <aniruddhasvarma@gmail.com>
2026-06-29 13:04:44 +0530

    install script new

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ if ! command -v ros2 &> /dev/null; then
     sudo add-apt-repository universe -y
     sudo apt-get update && sudo apt-get install curl -y
     sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://mirror.umd.edu/packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
     sudo apt-get update
     sudo apt-get install -y ros-$ROS_DISTRO-desktop python3-argcomplete python3-colcon-common-extensions python3-rosdep python3-vcstool
 else
